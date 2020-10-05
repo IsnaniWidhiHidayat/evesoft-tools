b2aca8a8f5caa2ac33cab3bf315d129fca9783d7
isnani widhi hidayat <isnani.hidayat@gmail.com>
2020-10-05 23:50:39 +0700

    Change Menu

--- Runtime/Menu.cs
+++ Runtime/Menu.cs
@@ -8,7 +8,9 @@ namespace Evesoft
         public const string utils = root + separator + nameof(Evesoft.Utils);
         public const string input = root + separator + nameof(Evesoft.Input);
         public const string views = root + separator + nameof(Evesoft.Views);
+        #if LOCALIZE
         public const string localize = root + separator + nameof(Evesoft.Localize);
+        #endif
         public const string Dialogue = root + separator + nameof(Dialogue);
     }
 }
